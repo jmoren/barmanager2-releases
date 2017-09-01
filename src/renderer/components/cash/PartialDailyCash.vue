d676e33ee6b5ea46dd2c28464e2a3cd6365c81af
Jairo de Morais <jairodemorais@gmail.com>
2017-09-01 18:45:17 -0300

    fix issue discounting 2 times the total home

--- src/renderer/components/cash/PartialDailyCash.vue
+++ src/renderer/components/cash/PartialDailyCash.vue
@@ -371,7 +371,7 @@
         return parseFloat(this.cash.cash_amount) + parseFloat(this.cash.debt_cash)
       },
       realCash () {
-        return parseFloat(this.subCard) + parseFloat(this.subCash) + parseFloat(this.cash.init_amount) - parseFloat(this.debitos) - parseFloat(this.cash.total_home) + parseFloat(this.totalInFavor)
+        return parseFloat(this.subCard) + parseFloat(this.subCash) + parseFloat(this.cash.init_amount) - parseFloat(this.debitos) + parseFloat(this.totalInFavor)
       },
       onlyCash () {
         return parseFloat(this.cash.favor_cash) + parseFloat(this.cash.init_amount) + parseFloat(this.subCash) - parseFloat(this.debitos) - parseFloat(this.cash.cash_at_home)
