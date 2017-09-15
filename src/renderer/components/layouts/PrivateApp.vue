fa8e1292edb23716b208ad18c849fdfcfe43493e
Jairo de Morais <jairodemorais@gmail.com>
2017-09-15 14:14:18 -0300

    change default font to Arial

--- src/renderer/components/layouts/PrivateApp.vue
+++ src/renderer/components/layouts/PrivateApp.vue
@@ -352,7 +352,7 @@
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
